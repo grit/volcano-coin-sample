10f7f9b7bbc8e69f1226518e8aa040c741925c52
proxyGrit <dev@proxygrit.com>
2022-04-13 00:00:28 -0700

    Update deploy script for contract

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -3,7 +3,7 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+const hre = require('hardhat');
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -14,19 +14,19 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const VolcanoCoin = await hre.ethers.getContractFactory('VolcanoCoin');
+  const volcanoCoin = await VolcanoCoin.deploy();
 
-  await greeter.deployed();
+  await volcanoCoin.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log('VolcanoCoin deployed to:', volcanoCoin.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
   .then(() => process.exit(0))
-  .catch((error) => {
+  .catch(error => {
     console.error(error);
     process.exit(1);
   });
